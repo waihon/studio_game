6ba4f11c1ad6ed224cc2067c2cd7e94bbef38e24
Waihon Yew <yewwaihon@gmail.com>
2015-05-20 00:04:21 +0800

    Finish 3 - Numbers and Strings

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,10 @@
-greeting = "Welcome!"
-
-3.times { puts greeting.upcase }
-
-puts Time.now
+name1 = "larry"
+health1 = 60
+puts name1 + '\'s health is ' + health1.to_s
+puts "#{name1}'s health is #{health1}"
+puts "#{name1}'s health is #{health1 * 3}"
+puts
+name2 = "curly"
+name3 = "moe"
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
 
